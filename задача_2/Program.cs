a1bffd480363c98efb039f45abb97e930af0e622
Valeria <lerka.paradova@mail.ru>
2022-08-20 00:26:31 +0300

    внесла новую папку

---
+++ задача_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает
+// два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+System.Console.Write("Enter first number : ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Enter second number : ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
