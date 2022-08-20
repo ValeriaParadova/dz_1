d61798f888a28ddc94be962192801818a26ed342
Valeria <lerka.paradova@mail.ru>
2022-08-20 21:04:19 +0300

    добавила папку 6

---
+++ задача_6/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+// Например:
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+System.Console.Write("Enter number : ");
+ 
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number % 2 == 0)
+{ 
+    Console.WriteLine( "Yes");   
+}
+else
+{
+    Console.WriteLine( "No");     
+}
