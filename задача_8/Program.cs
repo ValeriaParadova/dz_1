d12412fac54ccdf6ced52c7d9797321b364bb948
Valeria <lerka.paradova@mail.ru>
2022-08-24 20:28:07 +0300

    add diff1

--- задача_8/Program.cs
+++ задача_8/Program.cs
@@ -4,18 +4,45 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
+// Первый способ решения задачи:
+
+// Console.Write("Enter number : ");
+
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int i = 2;
+
+//     while (i <= number)
+// {
+//     if (i % 2 == 0)
+// {
+//  Console.Write(i + " ");
+
+// }
+// i++;
+// }
+
+
+// Второй способ решения задачи через цикл for:
+
+// Console.Write("Enter number : ");
+// int Number1 = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = 2; i <= Number1; i+=2 )
+// {
+// Console.Write(i);
+// }
+
+// Третий способ решения задачи через цикл for:
+
 Console.Write("Enter number : ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
 
-int number = Convert.ToInt32(Console.ReadLine());
-
-int i = 2;
-
-    while (i <= number)
+for (int i = 2; i <= Number1; i+=2 )
 {
-    if (i % 2 == 0)
-{
- Console.Write(i + " ");
-
+    System.Console.Write(i+ " ");
 }
-i++;
+for (int i = 1; i <= Number1; ++i)
+{
+    if (i % 2 == 0) System.Console.Write(i + " ");
 }
